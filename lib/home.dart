15337d38fffca9deba2c89100b0b1d5729ee3525
aldiramadani00 <alditipex123@gmail.com>
2023-05-23 18:58:00 +0700

    Flutter
    1

--- lib/home.dart
+++ lib/home.dart
@@ -1,11 +1,19 @@
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Halaman 2"),
+      ),
+      body: ElevatedButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          child: const Text("kembali")),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,32 @@
 import 'package:flutter/material.dart';
+import 'home.dart';
 
 void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: Scaffold(
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {},
-          child: const Icon(Icons.plus_one),
-        ),
-        body: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            children: [
-              Text("ini halaman pertama"),
-              const TextField(
-                decoration: InputDecoration(labelText: "Username"),
-              ),
-              const TextField(
-                obscureText: true,
-                decoration: InputDecoration(labelText: "Password"),
-              ),
-              const SizedBox(
-                height: 10.0,
-              ),
-              ElevatedButton(
-                onPressed: () {},
-                child: const Text("Login"),
-              ),
-              ElevatedButton(
-                onPressed: () {},
-                child: Text("Pindah halaman 2"),
-              ),
-            ],
-          ),
-        ),
-        appBar: AppBar(
-          title: const Text("Rumah Kebab & Burger"),
-        )),
+  runApp(const MaterialApp(
+    home: MyApp(),
   ));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Navigation"),
+      ),
+      body: Column(
+        children: [
+          const Text("Ini Halaman Pertama"),
+          ElevatedButton(
+              onPressed: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => const Home()));
+              },
+              child: const Text("Pindah Halaman 2")),
+        ],
+      ),
+    );
+  }
 }
